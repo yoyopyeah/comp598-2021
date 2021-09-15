7fe08a0ed2eda1335857e137be3af834ca945b8c
Yoyo Peng <yoyopyeah@gmail.com>
2021-09-14 22:14:38 -0400

    data filtered

--- hw1/submission_template/src/test.py
+++ hw1/submission_template/src/test.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
+# read data
 df = pd.read_csv("../data/IRAhandle_tweets_1.csv")
-print(df)
+
+# filter data
+df = df.head(1000)[(df["language"] == "English") & (df["content"].str.contains('?', regex=False)==False)]
+
+# write filtered data to new tsv file
+df.to_csv('../data/filtered_tweets.tsv', sep = '\t', index=False)
