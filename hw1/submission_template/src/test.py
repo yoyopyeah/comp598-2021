5c1f312151dddb6318758a7a66e5f97d741b19b0
Yoyo Peng <yoyopyeah@gmail.com>
2021-09-15 01:03:17 -0400

    data annotated

--- hw1/submission_template/src/test.py
+++ hw1/submission_template/src/test.py
@@ -1,10 +1,22 @@
 import pandas as pd
+import numpy as np
 
 # read data
 df = pd.read_csv("../data/IRAhandle_tweets_1.csv")
 
 # filter data
-df = df.head(1000)[(df["language"] == "English") & (df["content"].str.contains('?', regex=False)==False)]
+df = df.head(1000)[(df["language"] == "English") & (df["content"].str.contains('\?')==False)]
 
-# write filtered data to new tsv file
-df.to_csv('../data/filtered_tweets.tsv', sep = '\t', index=False)
+# data annotation
+df['trump_mention'] = np.where(df['content'].str.contains(r'[^a-zA-Z0-9]Trump[^a-zA-Z0-9]'), 'T', 'F')
+
+# write filtered & annotated data to new tsv file
+header = ["tweet_id", "publish_date", "content", "trump_mention"]
+df.to_csv('../data/dataset.tsv', sep = '\t', columns = header)
+
+# analyze and write data to new tsv file
+header = ["result", "value"]
+
+
+# print(df.loc[119:120, ['content', 'trump_mention']])
+# print(df['trump_mention'].value_counts())
