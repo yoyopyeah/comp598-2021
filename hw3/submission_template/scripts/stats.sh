6f9d6d41260baff45bf9fe9b8106b56d4ee86652
Yoyo Peng <yoyopyeah@gmail.com>
2021-09-29 18:35:47 -0400

    done

---
+++ hw3/submission_template/scripts/stats.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# number of lines
+LINES=$(cat $1 | wc -l)
+
+# error when line < 10000
+if [[ LINES -lt 1000 ]];
+then 
+    echo "Error! The input file is smaller than 10,000 lines." 1>&2  
+    exit 1
+else
+    echo $LINES
+fi
+
+# header row
+head -n 1 $1
+
+# -i "potus"
+echo $(tail -n 10000 $1 | grep -i "potus" | wc -l)
+
+# "word"
+echo $(sed -n '100,200p' < $1 | grep "fake" | wc -l)
+
---
+++ hw3/submission_template/src/dialog_analysis.py
@@ -0,0 +1,47 @@
+import json
+import csv
+
+# base structure
+base = {
+    "count": {
+        "twilight sparkle": 0,
+        "applejack": 0,
+        "rarity": 0,
+        "pinkie pie": 0,
+        "rainbow dash": 0,
+        "fluttershy": 0
+    },
+    "verbosity": {
+        "twilight sparkle": 0,
+        "applejack": 0,
+        "rarity": 0,
+        "pinkie pie": 0,
+        "rainbow dash": 0,
+        "fluttershy": 0
+    } 
+}
+
+total = 0
+
+with open ('../data/clean_dialog.csv') as data:
+    reader = csv.reader(data, delimiter=',')
+    for row in reader:
+        for pony in base["count"]:
+            if pony in row[2].lower():
+                base["count"][pony] += 1
+                total += 1
+    for pony in base["verbosity"]:
+        base["verbosity"][pony] = round(base["count"][pony] / total, 2)
+
+
+
+# result = json.dumps(base)
+
+# with open ('../output.json', 'w') as outfile:
+#     json.dump(base, outfile)
+#     print(json.dumps(base, indent=1))
+
+f = open("../output.json", "w")
+f.write(json.dumps(base, indent=1))
+f.close()
+
